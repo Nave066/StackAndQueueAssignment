31728305859ac52790f0f2a0435a217530b6162f
Nave066 <jayamnaveen68@gmail.com>
2022-01-24 11:35:32 +0530

    Stack and queue assignment

---
+++ QueueAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace QueueAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue Using Linked List");
+            LinkedListUsingQueue linkedListQueue = new LinkedListUsingQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            Console.WriteLine("The Queue is :");
+            linkedListQueue.Display();
+            Console.WriteLine("\n---------------------\n");
+            Console.WriteLine("Performing Dequeue Operation till queue got empty.....");
+            linkedListQueue.Dequeue();
+            linkedListQueue.Display();
+            Console.WriteLine("\n---------------------\n");
+            linkedListQueue.Dequeue();            
+            linkedListQueue.Display();
+            Console.WriteLine("\n---------------------\n");
+            linkedListQueue.Dequeue();
+            linkedListQueue.Display();
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ StackAndQueueAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StackAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            LinkedListUsingStack linkedListStack = new LinkedListUsingStack();
+            linkedListStack.Push(57);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+            linkedListStack.Display();
+            linkedListStack.Peek(); //top.data;
+            linkedListStack.Pop();
+            linkedListStack.MakeEmpty();
+            linkedListStack.Display();
+            Console.ReadKey();
+        }
+    }
+}
